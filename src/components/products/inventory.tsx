8497d35a40b77bc46e576bd508ea33369a966049
natanarau <natan.mca@gmail.com>
2022-06-07 16:56:10 -0300

    Colocando o projeto no ar

--- src/components/products/inventory.tsx
+++ src/components/products/inventory.tsx
@@ -4,7 +4,7 @@ export default function Inventory(idProduct: number) {
   data.find(e => {    
     if(e.id === idProduct) {
      // FAZER ESQUEMA DE ADICIONAR AO CARRINHO...
-     console.log(`FAZER ESQUEMA PARA ADICIONAR PRODUTO DE ID ${e.id} AO CARRINHO NO LOCALSTORAGE`)
+     console.log(`FAZER CONTAGEM PARA ADICIONAR PRODUTO DE ID ${e.id} AO CARRINHO NO LOCALSTORAGE`)
     }
   });
 }
